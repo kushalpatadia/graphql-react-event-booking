c010331232a7b411fe3f8a194a30b5277847e10a
Kushal Patadia <kushalpatadia@gmail.com>
2022-07-06 18:20:45 +0530

    Step3: Created basic event Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hey you done it!!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name:String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Watching movies", "Table Tennise"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.get("/",(req,res,next)=>{
+  res.send("hey")
+})
 app.listen(3000);
