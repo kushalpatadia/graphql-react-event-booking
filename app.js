ba06def72480839597cb68c5eadc88fee71989ab
Kushal Patadia <kushalpatadia@gmail.com>
2022-07-07 11:03:53 +0530

    Step 5 : Connect moodb and apply all create and fetch logic from mongodb

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 app.use(bodyParser.json());
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema: buildSchema(`
       type Event {
@@ -42,25 +42,45 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: args.eventInput.price,
-          date: args.eventInput.date,
-        }
-        events.push(event)
-        return event;
-      },
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then((result) => {
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
-    graphiql: true,
+    graphiql: true
   })
 );
 
-app.get("/", (req, res, next) => {
-  res.send("hey");
-});
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.brbk4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log('🚀 ~ file: app.js ~ line 73 ~ err', err);
+  });
